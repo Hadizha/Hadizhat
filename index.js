ba56ecce1377e770f86f5990e21a78270bf018a4
Hadizha <51534498+Hadizha@users.noreply.github.com>
2019-06-20 20:42:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 function humburgerClick () {
-    var mobileMenu = document.getElementsByClassName("mobile-menu") [0];
-    var display = mobileMenu.style.display;
-    if(display == ""){
-        mobileMenu.style.display = "flex";
-    }else{
-        mobileMenu.style.display = "";
-    }
+    $(".mobile-menu").slideToggle(600);
 }
-function moreClick(event){
+function moreClick(event, cardNumber){
     event.preventDefault();
-    var itemWindow = document.getElementsByClassName("item-window") [0];
+    cardNumber = cardNumber || "";
+    var itemWindow = document.querySelector(".item-window" + cardNumber);
     var display = itemWindow.style.display;
-    if(display == ""){
-        itemWindow.style.display = "flex"        
+    if(display == "" || display == "none"){
+        $(".item-window" + cardNumber).fadeIn(600);
+        itemWindow.style.display = "flex";       
     }else{
-        itemWindow .style.display = "";
+        $(".item-window").fadeOut(600) ;
     }
 }
 var humburgerIcon = document.getElementsByClassName ("menu-icon") [0];
 humburgerIcon.addEventListener("click", humburgerClick);
 
-var moreIcon = document.getElementsByClassName ("card-button") [1];
-moreIcon.addEventListener("click", moreClick);
+//var moreIcon = document.getElementsByClassName ("card-button") [1];
+//moreIcon.addEventListener("click", moreClick);
 
-var closeIcon = document.getElementsByClassName ("item-close") [0];
-closeIcon.addEventListener("click", moreClick);
+//var closeIcon = document.getElementsByClassName ("item-close") [0];
+//closeIcon.addEventListener("click", moreClick);
+$(".item-close").click(
+    function(){
+$(".item-window").fadeOut();
+    }
+);
+
+$(".card-button.first").click(
+    function(event){
+        moreClick(event,".first");
+    }
+);
+$(".card-button.second").click(
+    function(event){
+        moreClick(event,".second");
+    }
+);
+$(".card-button.third").click(
+    function(event){
+        moreClick(event,".third");
+    }
+);
+$(".card-button.fourth").click(
+    function(event){
+        moreClick(event,".fourth");
+    }
+);
+
+$("a[href='#']").click(
+    function (){
+        $("html, body").animate({
+            scrollTop: 0
+    },1000);
+    });
